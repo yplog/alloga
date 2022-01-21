bdee2a8a92cc5f8f3b7b12136eedefaf93f6cabc
yplog <yalinpala@protonmail.com>
2022-01-21 13:33:41 +0300

    player dash destroy enemies

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,6 +4,8 @@ signal died
 
 enum State { NORMAL, DASHING }
 
+export(int, LAYERS_2D_PHYSICS) var dashHazardMask
+
 const GRAVITY = 1000
 var maxHorizontalSpeed = 140
 var jumpSpeed = 500
@@ -17,9 +19,12 @@ var minDashSpeed = 200
 var currentState = State.NORMAL
 var isStateNew = true
 
+var defaultHazardMask = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
+	defaultHazardMask = $HazardArea.collision_mask
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,6 +44,10 @@ func change_state(newState):
 
 
 func process_normal(delta):
+	if (isStateNew):
+		$DashArea/CollisionShape2D.disabled = true
+		$HazardArea.collision_mask = defaultHazardMask
+
 	var moveVector = get_movement_vector()
 	
 	velocity.x = moveVector.x * horizontalAcceleration * delta
@@ -79,7 +88,9 @@ func process_normal(delta):
 
 func process_dash(delta):
 	if (isStateNew):
+		$DashArea/CollisionShape2D.disabled = false
 		$AnimatedSprite.play("jump")
+		$HazardArea.collision_mask = dashHazardMask
 		var moveVector = get_movement_vector()
 		var velocityMod = 1
 		if (moveVector.x != 0):
